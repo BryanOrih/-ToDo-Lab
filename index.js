c4bccf40333c6b9c18f47a332087cd5b40e0dda6
Bryan Alvarez <balvarez@LABA.PSWAS.LOCAL>
2023-05-05 16:04:39 -0700

    updating

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 const form = document.querySelector("form")
+const ul = document.querySelector('ul')
 
 class TodoItem{
-    constructor(id, text, isCompleted, element){
+    constructor(id, text, isCompleted){
         this.id = id;
         this.text = text.textContent;
         this.isCompleted = isCompleted
-        this.element = element
+        this.element = this.createDOMElement();
     }
     createDOMElement(){
-        let newTodoElement = document.createElement('div');
-        newTodoElement.classList.add("todo");
-        let todoInput = document.createElement('input');
-        todoInput.type = "checkbox";
-        todoInput.classList.add('todo-checkbox')
-        newTodoElement.appendChild(todoInput) 
+
+        const li = document.createElement('li');
+        //SECTION - CREATES THE CHECKBOX AND MAKES IT RELATED TO THE ISCOMPLETE VARIABLE
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = this.isCompleted;
+        //REVIEW - ASSUMING THAT IT SHOULD LINK TO UPDATE DOM IF CLICKED
+        checkbox.addEventListener('click', () => {
+          this.isCompleted = !this.isCompleted;
+          this.updateDOMElement();
+        });
+        //SECTION - PUTS CHECKBOX INSIDE LIST
+        li.appendChild(checkbox);
+        //SECTION - 
+        const label = document.createElement('label');
+        label.textContent = this.text;
+        li.appendChild(label);
+        //SECTION - CREATE BUTTON
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        //
+        deleteButton.addEventListener('click', () => {
+            
+        });
+        li.appendChild(deleteButton);
+        
+        return li;
     }
     updateDOMElement(){
+
+        const checkbox = this.element.querySelector('input[type="checkbox"]');
+        checkbox.checked = this.isCompleted;
+        const label = this.element.querySelector('label');
+        label.textContent = this.text;
 
     }
 }
 class TodoList{
-    constructor(items, element){
-        this.items = items;
+    constructor(element){
+        this.items = [];
         this.element = element;
     }
-    static add(text){
-
+    add(text){
+        let newToDo = new TodoItem(Math.random(), text, false)
+        this.items.push(newToDo)
+        this.element.innerHTML += `<li>${text}</li>`
     }
     remove(id){
 
@@ -34,6 +63,12 @@ class TodoList{
 
     }
 }
+
+// list.appendChild(TodoItem.createDOMElement())
+let homeToDo = new TodoList(ul)
+
+homeToDo.add("Do Laundry")
+
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
